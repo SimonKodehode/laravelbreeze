35fe15a1c3f8ddff0eb611f1b3ed1c952305e445
MadsKodehode <mads.kodehode@gmail.com>
2023-02-19 15:13:34 +0100

    Responisve dashboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,14 +1,42 @@
-<script setup>
+<script>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head } from "@inertiajs/vue3";
+import { Head, Link } from "@inertiajs/vue3";
 import AppLayout from "@/Layouts/AppLayout.vue";
+import BlogCard from "@/Components/BlogCard.vue";
+
+export default {
+    components: {
+        BlogCard,
+        Link,
+        Head,
+        AppLayout,
+    },
+
+    methods: {
+        deleteBlog(id) {
+            if (confirm("Are you sure?")) {
+                this.$inertia.delete(this.route(""));
+            }
+        },
+    },
+};
 </script>
 
 <template>
     <Head title="Dashboard" />
     <AppLayout>
-        <div>
-            <h1>Welcome {{ $page.props.auth.user.name }}!</h1>
-        </div>
+        <section class="dashboard">
+            <h1 class="dashboard-headline">
+                Welcome {{ $page.props.auth.user.name }}!
+            </h1>
+
+            <div class="dashboard-blog">
+                <header class="dashboard-blog-header">
+                    <p class="dashboard-blog-id">POSTID</p>
+                    <button class="dashboard-blog-delete">DELETE</button>
+                </header>
+                <BlogCard></BlogCard>
+            </div>
+        </section>
     </AppLayout>
 </template>
