efd9b3cdc1a9a9f26e7a3a463b959cdddd7a6ff5
Simon Kodehode <simonv.kodehode@gmail.com>
2023-02-24 13:28:00 +0100

    added redirect on create if user is not logged in

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -31,6 +31,10 @@ class BlogPostController extends Controller
      */
     public function create()
     {
+      if (!auth()->check()) {
+        return redirect()->route('login');
+      }
+
       return Inertia::render('NewBlog');
     }
 
