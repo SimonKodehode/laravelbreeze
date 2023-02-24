cd09349483e76f29ec671650b9813fe6e6145fe6
Simon Kodehode <simonv.kodehode@gmail.com>
2023-02-24 13:25:28 +0100

    added unique slug in the blogpost model

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,12 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class BlogPost extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'body', 'image', 'slug'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            $post->slug = Str::slug($post->title, '-');
+            $latestSlug =
+                static::whereRaw("slug RLIKE '^{$post->slug}(-[0-9]*)?$'")
+                ->latest('id')
+                ->value('slug');
+            if ($latestSlug) {
+                $pieces = explode('-', $latestSlug);
+                $number = intval(end($pieces));
+                $post->slug .= '-' . ($number + 1);
+            }
+        });
+    }
 
     public function user()
     {
